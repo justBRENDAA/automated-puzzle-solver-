d3e9dca630fde8a0ddc48432ac0bfbba6d767d44
Brenda Luis <brendaa.scholar@gmail.com>
2025-09-12 17:52:04 -0500

    Modifications to slicing logic

--- src/image_functions.py
+++ src/image_functions.py
@@ -7,4 +7,14 @@ def split_image(image, num_rows, num_cols):
     tile_height = h // num_rows
     tile_width = w // num_cols
     
-    # return tiles
+    for row in range(num_rows):
+        for col in range(num_cols):
+            top = row * tile_height
+            bottom = (row + 1) * tile_height if row != num_rows - 1 else h
+            left = col * tile_width
+            right = (col + 1) * tile_width if col != num_cols - 1 else w
+            
+            tile = image[top:bottom, left:right]
+            tiles.append(tile)
+
+    return tiles
