4f79673d82b0ac4cf26ca3a1831dece5439baf54
Brenda Luis <brendaa.scholar@gmail.com>
2025-09-12 18:48:56 -0500

    Image_functions.py logic modification

--- src/image_functions.py
+++ src/image_functions.py
@@ -1,20 +1,14 @@
 import cv2
 import numpy as np
 
-def split_image(image, num_rows, num_cols):
-    tiles = []
+def split_image(image, rows, cols):
     h, w = image.shape[:2]
-    tile_height = h // num_rows
-    tile_width = w // num_cols
-    
-    for row in range(num_rows):
-        for col in range(num_cols):
-            top = row * tile_height
-            bottom = (row + 1) * tile_height if row != num_rows - 1 else h
-            left = col * tile_width
-            right = (col + 1) * tile_width if col != num_cols - 1 else w
-            
-            tile = image[top:bottom, left:right]
-            tiles.append(tile)
+    print("height: ", h)
+    print("width: ", w)
 
-    return tiles
+    slice_height = h//rows
+    slice_width = w//cols
+
+    print("slice height: ", slice_height)
+    print("slice width: ", slice_height)
+
