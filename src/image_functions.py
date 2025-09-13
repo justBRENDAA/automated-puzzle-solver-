f4880e608450146d8749031ac1951d4c1af28867
Brenda Luis <brendaa.scholar@gmail.com>
2025-09-12 20:14:44 -0500

    split_image now reusable

--- src/image_functions.py
+++ src/image_functions.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 def split_image(image, rows, cols):
     h, w = image.shape[:2]
@@ -9,6 +10,27 @@ def split_image(image, rows, cols):
     slice_height = h//rows
     slice_width = w//cols
 
-    print("slice height: ", slice_height)
-    print("slice width: ", slice_height)
+    print("slice height: ", slice_height) 
+    print("slice width: ", slice_width)
+    print()
 
+    slices = []
+
+    for row in range(rows):
+        for col in range(cols):
+            print(f"Slice: {row, col}")
+
+            height_start = row * slice_height
+            height_end =  (row + 1) * slice_height if row < rows - 1 else h  
+
+            width_start = col * slice_width
+            width_end =  (col + 1) * slice_width if col < cols - 1 else w
+
+            print(f"Height slice values: {height_start} : {height_end}")
+            print(f"Width slice values: {width_start} : {width_end}")
+            print()
+
+            slice = image[height_start:height_end, width_start:width_end]
+            plt.imshow(slice)
+            plt.axis("off")
+            plt.show()
